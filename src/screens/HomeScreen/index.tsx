74e30bdc36afc84dfef1d6ca36125483e7193d3a
pdimarcodev <pablo.d.dimarco@gmail.com>
2021-10-05 14:24:29 -0300

    readme file

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {styles} from './styles';
 
 const HomeScreen = () => {
   return (
